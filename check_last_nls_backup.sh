6588269af4fb8de18f3cfe877f58069d6b03d7e5
Jorgen <jur.public@xs4all.nl>
2016-07-19 14:32:45 +0200

    Update check_last_nls_backup.sh

--- check_last_nls_backup.sh
+++ check_last_nls_backup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-# check_last_nls_backup.sh
-# version 0.1
+# Script:       check_last_nls_backup.sh
+# Purpose:      Check most recent Elasticsearch/Nagios Log Server Backup - tested with curator 3.40
+# Version:      0.1 initial creation
+# Version:      0.2 changed --time-unit from 1 days to 25 hours, it appeared that a backup made yesterday evening (23:30) did not match filter "--newer-than 1 --time-unit days",  I assumed a last 24 hours filter. No matches
 LAST=$(curator --loglevel warn show snapshots --repository "SharedBackupRepo" --newer-than 25 --time-unit hours  |tail -1)
 RESULT=$(curl -s -XGET "localhost:9200/_snapshot/SharedBackupRepo/${LAST}?pretty" | awk -F\" '/state/ {print $4}')
 
@@ -24,7 +26,7 @@ case ${RESULT} in
                 exit 2
                 ;;
         *)
-                echo "UNKNOWN: Last backup not successful. Please verify curator session $LAST ${RESULT}"
+                echo "UNKNOWN: Last backup not successful. Please verify! Result: $LAST ${RESULT}"
                 exit 3
                 ;;
 esac
