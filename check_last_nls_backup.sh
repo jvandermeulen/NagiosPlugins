effba9151d7c5922cddfeb0270f46be36d3bd033
Jorgen <jur.public@xs4all.nl>
2016-07-22 10:14:21 +0200

    Update check_last_nls_backup.sh
    add reason for failure or partion as multiline output

--- check_last_nls_backup.sh
+++ check_last_nls_backup.sh
@@ -4,6 +4,7 @@
 # Purpose:      Check most recent Elasticsearch/Nagios Log Server Backup - tested with curator 3.40
 # Version:      0.1 initial creation
 # Version:      0.2 changed --time-unit from 1 days to 25 hours, it appeared that a backup made yesterday evening (23:30) did not match filter "--newer-than 1 --time-unit days",  I assumed a last 24 hours filter. No matches
+# Version:      0.3 add multiline output to PARTIAL or FAILED backups: reason for failure
 LAST=$(curator --loglevel warn show snapshots --repository "SharedBackupRepo" --newer-than 25 --time-unit hours  |tail -1)
 RESULT=$(curl -s -XGET "localhost:9200/_snapshot/SharedBackupRepo/${LAST}?pretty" | awk -F\" '/state/ {print $4}')
 
@@ -18,11 +19,13 @@ case ${RESULT} in
                 exit 0
                 ;;
         PARTIAL*)
-                echo "WARNING: Last backup not fully successful but ${RESULT}"
+                REASON=$(curl -s -XGET "localhost:9200/_snapshot/SharedBackupRepo/${LAST}?pretty" | awk -F \" '/reason/ {print $4}')
+                echo -e "WARNING: Last backup not fully successful but  ${RESULT}\n${REASON}"
                 exit 1
                 ;;
         FAILED)
-                echo "CRITICAL: Last backup FAILED. Please verify curator session $LAST ${RESULT}"
+                REASON=$(curl -s -XGET "localhost:9200/_snapshot/SharedBackupRepo/${LAST}?pretty" | awk -F \" '/reason/ {print $4}')
+                echo -e "CRITICAL: Last backup FAILED. Please verify curator session $LAST ${RESULT}\n${REASON}"
                 exit 2
                 ;;
         *)
