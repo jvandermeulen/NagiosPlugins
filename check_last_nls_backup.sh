25bb9fdf4cf21a90429da0e51eb42ec2c55ce20e
Jorgen <jur.public@xs4all.nl>
2016-07-22 10:33:27 +0200

    Update check_last_nls_backup.sh
    make multiline output configurable

--- check_last_nls_backup.sh
+++ check_last_nls_backup.sh
@@ -5,6 +5,15 @@
 # Version:      0.1 initial creation
 # Version:      0.2 changed --time-unit from 1 days to 25 hours, it appeared that a backup made yesterday evening (23:30) did not match filter "--newer-than 1 --time-unit days",  I assumed a last 24 hours filter. No matches
 # Version:      0.3 add multiline output to PARTIAL or FAILED backups: reason for failure
+# Version:      0.5 it appears that NRDS Version: 1.5.4 Date: 05/06/2016  cannot handle multiline output, so make it configurable
+
+
+# Customize this:
+# MULTILINE: make it eiter an empty variable or "\n"
+#MULTILINE="\n"
+MULTILINE=""
+# End customizations
+
 LAST=$(curator --loglevel warn show snapshots --repository "SharedBackupRepo" --newer-than 25 --time-unit hours  |tail -1)
 RESULT=$(curl -s -XGET "localhost:9200/_snapshot/SharedBackupRepo/${LAST}?pretty" | awk -F\" '/state/ {print $4}')
 
@@ -19,12 +28,12 @@ case ${RESULT} in
                 exit 0
                 ;;
         PARTIAL*)
-                REASON=$(curl -s -XGET "localhost:9200/_snapshot/SharedBackupRepo/${LAST}?pretty" | awk -F \" '/reason/ {print $4}')
+                REASON=$(echo ${MULTILINE};curl -s -XGET "localhost:9200/_snapshot/SharedBackupRepo/${LAST}?pretty" | awk -F \" '/reason/ {print $4}')
                 echo -e "WARNING: Last backup not fully successful but  ${RESULT}\n${REASON}"
                 exit 1
                 ;;
         FAILED)
-                REASON=$(curl -s -XGET "localhost:9200/_snapshot/SharedBackupRepo/${LAST}?pretty" | awk -F \" '/reason/ {print $4}')
+                REASON=$(echo ${MULTILINE};curl -s -XGET "localhost:9200/_snapshot/SharedBackupRepo/${LAST}?pretty" | awk -F \" '/reason/ {print $4}')
                 echo -e "CRITICAL: Last backup FAILED. Please verify curator session $LAST ${RESULT}\n${REASON}"
                 exit 2
                 ;;
