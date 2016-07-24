b031175fce5702540b77725d37b88d87ca0e7d1e
Jorgen <jur.public@xs4all.nl>
2016-07-24 17:19:07 +0200

    Update check_last_nls_backup.sh

--- check_last_nls_backup.sh
+++ check_last_nls_backup.sh
@@ -3,23 +3,26 @@
 # Script:       check_last_nls_backup.sh
 # Purpose:      Check most recent Elasticsearch/Nagios Log Server Backup - tested with curator 3.40
 # Version:      0.1 initial creation
-# Version:      0.2 changed --time-unit from 1 days to 25 hours, it appeared that a backup made yesterday evening (23:30) did not match filter "--newer-than 1 --time-unit days",  I assumed a last 24 hours filter. No matches
-# Version:      0.3 add multiline output to PARTIAL or FAILED backups: reason for failure
-# Version:      0.5 it appears that NRDS Version: 1.5.4 Date: 05/06/2016  cannot handle multiline output, so make it configurable
-# Idea:         date -d "$(echo ${D:0:8} ${D:(-6):2}:${D:(-4):2}:${D:(-2):2})"
+#               0.2 changed --time-unit from 1 days to 25 hours, it appeared that a backup made yesterday evening (23:30) did not match filter "--newer-than 1 --time-unit days",  I assumed a last 24 hours filter. No matches
+#               0.3 add multiline output to PARTIAL or FAILED backups: reason for failure
+#               0.5 it appears that NRDS Version: 1.5.4 Date: 05/06/2016  cannot handle multiline output, so make it configurable
+#               0.6 added readable date
 
 
 # Customize this:
 # MULTILINE: make it eiter an empty variable or "\n"
 #MULTILINE="\n"
 MULTILINE=""
+
+# CURATOR_TIME_CORRECTION
+CURATOR_TIME_CORRECTION=" +0000"
 # End customizations
 
 LAST=$(curator --loglevel warn show snapshots --repository "SharedBackupRepo" --newer-than 25 --time-unit hours  |tail -1)
 RESULT=$(curl -s -XGET "localhost:9200/_snapshot/SharedBackupRepo/${LAST}?pretty" | awk -F\" '/state/ {print $4}')
 
 D=$(echo ${LAST} | awk -F\- '{print $2}')
-PRETTY_TIMESTAMP=$(date -d "$(echo ${D:0:8} ${D:(-6):2}:${D:(-4):2}:${D:(-2):2})")
+PRETTY_TIMESTAMP=$(date -d "$(echo ${D:0:8} ${D:(-6):2}:${D:(-4):2}:${D:(-2):2})${CURATOR_TIME_CORRECTION}")
 
 # debug
 #echo LAST=${LAST}, RESULT=${RESULT}.
