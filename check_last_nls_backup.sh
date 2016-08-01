1258673d03cbe2502e81de576985f7f74ee89e51
Jorgen <jur.public@xs4all.nl>
2016-08-01 11:34:38 +0200

    version 0.8 MYREPO
     0.8 oops.. name of backup repository was hardcoded in this script, made it configurable using variable MYREPO

--- check_last_nls_backup.sh
+++ check_last_nls_backup.sh
@@ -9,7 +9,7 @@
 #               0.5 it appears that NRDS Version: 1.5.4 Date: 05/06/2016  cannot handle multiline output, so make it configurable
 #               0.6 added readable date using "date -d <curatordate>, changed --newer-than and --time-unit parameters to variables to improve UNKOWN result with regards to timeframe
 #               0.7 added help and version info
-
+#               0.8 oops.. name of backup repository was hardcoded in this script, made it configurable using variable MYREPO
 #==================================================
 # Customize this:
 
@@ -24,6 +24,9 @@ MULTILINE=""
 # CURATOR_TIME_CORRECTION
 CURATOR_TIME_CORRECTION=" +0000"
 
+# MYREPO: name of repository that should be checked using curator
+MYREPO="SharedBackupRepo"
+
 # debug
 #set -x
 # End customizations
@@ -31,7 +34,7 @@ CURATOR_TIME_CORRECTION=" +0000"
 
 function print_version () {
     cat <<EOF
-check_last_nls_backup.sh - 0.7 - Copyright Conclusion Xforce
+check_last_nls_backup.sh - 0.8 - Copyright Conclusion Xforce
 This Nagios plugin comes with no warranty. You can use and distribute it
 under terms of the GNU General Public License Version 2 (GPL V2) or later.
 EOF
@@ -44,12 +47,12 @@ Usage: check_last_nls_backup.sh -h, --help
 }
 
 function get_reason_for_failure () {
-REASON=$(curl -s -XGET "localhost:9200/_snapshot/SharedBackupRepo/${LAST}?pretty" | awk -F \" '/reason/ {print $4}')
+REASON=$(curl -s -XGET "localhost:9200/_snapshot/${MYREPO}/${LAST}?pretty" | awk -F \" '/reason/ {print $4}')
 }
 
 function main () {
-LAST=$(curator --loglevel warn show snapshots --repository "SharedBackupRepo" --newer-than ${MOSTRECENT_TIME_FILTER} --time-unit ${MOSTRECENT_TIME_UOM} |tail -1)
-RESULT=$(curl -s -XGET "localhost:9200/_snapshot/SharedBackupRepo/${LAST}?pretty" | awk -F\" '/state/ {print $4}')
+LAST=$(curator --loglevel warn show snapshots --repository "${MYREPO}" --newer-than ${MOSTRECENT_TIME_FILTER} --time-unit ${MOSTRECENT_TIME_UOM} |tail -1)
+RESULT=$(curl -s -XGET "localhost:9200/_snapshot/${MYREPO}/${LAST}?pretty" | awk -F\" '/state/ {print $4}')
 
 D=$(echo ${LAST} | awk -F\- '{print $2}' 2>/dev/null)
 [[ -n $D ]] || { echo "UNKNOWN: Unable to determine result within last ${MOSTRECENT_TIME_FILTER} ${MOSTRECENT_TIME_UOM}: $LAST ${RESULT}" ; exit 3; }
