eb2e0530cf2281fd3b24f691b9615af186b80b37
Jorgen <jur.public@xs4all.nl>
2018-03-21 21:17:08 +0100

    Create check_reboot_required.sh

---
+++ check_reboot_required.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Check if CentOS/RHEL server needs reboot
+# requirements: yum-utils package
+# version 0.1
+set -o pipefail
+/usr/bin/needs-restarting -r > /dev/null 2>&1
+RESULT=$?
+if [[ ${RESULT} -eq 1 ]]; then
+        echo "WARNING: Reboot is required. Updated libraries or kernel detected"
+        exit 1
+elif [[ ${RESULT} -eq 0 ]]; then
+        echo "OK: No system reboot required"
+        exit 0
+else
+        echo "UNKNOWN: $0 failed (is yum-utils installed?)"
+        exit 3
+fi
