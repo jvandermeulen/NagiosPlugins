2628249d2e1d6548f0ea789ddda985d7b64031ce
Jorgen <jur.public@xs4all.nl>
2019-02-09 01:39:02 +0100

    Added multiline output

--- check_reboot_required.bash
+++ check_reboot_required.bash
@@ -2,12 +2,12 @@
 # Check if CentOS/RHEL server needs reboot
 # requirements: yum-utils package
 #               el7
-# version 0.2
+# version 0.3
 set -o pipefail
 /usr/bin/needs-restarting -r > /dev/null 2>&1
 RESULT=$?
 if [[ ${RESULT} -eq 1 ]]; then
-        echo "WARNING: Reboot is required. Updated libraries or kernel detected"
+        echo -e "WARNING: Reboot is required. Updated libraries, service or kernel detected\nSee Red Hat article https://access.redhat.com/solutions/27943"
         exit 1
 elif [[ ${RESULT} -eq 0 ]]; then
         echo "OK: No system reboot required"
